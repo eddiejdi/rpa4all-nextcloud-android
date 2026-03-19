a0dbdbb26f08a75731b487bdb1a532bcc8aa80b3
eddiejdi <edenilson.teixeira@rpa4all.com>
2026-03-18 22:59:38 -0300

    Initial RPA4All customized Nextcloud Android

---
+++ app/src/rpa4all/java/com/nextcloud/client/di/VariantComponentsModule.java
@@ -0,0 +1,13 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 RPA4All
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
+ */
+package com.nextcloud.client.di;
+
+import dagger.Module;
+
+@Module
+abstract class VariantComponentsModule {
+}
